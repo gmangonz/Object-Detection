770f64d6110b9f2d3bf0325dc248cce5481b0648
gmangonz <germanurielgonzalez@gmail.com>
2023-06-09 17:48:22 -0500

    Update

--- src/setupData.py
+++ src/setupData.py
@@ -4,7 +4,33 @@ import tensorflow_datasets as tfds
 import os
 from .utils import Un_Normalize, Normalize, yxyx_to_yxhw
 
-def _parse_features(example_proto, image_shape, features):
+def if_resize_with_path(image, bbox, image_shape):
+
+    img_h = tf.shape(image)[0]
+    img_w = tf.shape(image)[1]
+    new_h = image_shape[0]
+    new_w = image_shape[1]
+    scale = tf.constant([new_h, new_w, new_h, new_w], dtype=tf.float32)
+
+    image = tf.image.resize_with_pad(image, image_shape[0], image_shape[1], method='nearest') / 255
+
+    # Calcualte the size of the new image that will be placed at the center of the "canvas" of height new_h and width new_w
+    h = tf.cast(img_h, tf.float32) * tf.cast(tf.math.minimum(new_w/img_w, new_h/img_h), tf.float32)
+    w = tf.cast(img_w, tf.float32) * tf.cast(tf.math.minimum(new_w/img_w, new_h/img_h), tf.float32)
+    ratio = tf.cast(tf.stack([h, w, h, w], axis=-1), dtype=bbox.dtype) / scale
+
+    # Calculate how much padding is needed
+    lower_h = (new_h-h)//2
+    lower_w = (new_w-w)//2
+
+    # Calculate the percentage that should be added to shift the bboxes correctly due to the padding
+    add_to_bboxes_h = lower_h/h
+    add_to_bboxes_w = lower_w/w
+
+    bbox = (bbox + tf.cast(tf.stack([add_to_bboxes_h, add_to_bboxes_w, add_to_bboxes_h, add_to_bboxes_w], axis=-1), bbox.dtype)) * ratio
+    return image, bbox
+
+def _parse_features(example_proto, image_shape, features, resize_with_path=True):
     
     parsed_features = tf.io.parse_single_example(example_proto, features)
     image = tf.image.decode_jpeg(parsed_features['image/encoded'], channels=3)
@@ -12,8 +38,8 @@ def _parse_features(example_proto, image_shape, features):
     h = tf.cast(parsed_features['image/height'], tf.int32)
     w = tf.cast(parsed_features['image/width'], tf.int32)
     image = tf.reshape(image, shape=[h, w, 3])
-    # image = tf.image.resize_with_pad(image, image_shape[0], image_shape[1]) / 255.
-    image = tf.image.resize(image, size=image_shape, method='nearest') / 255
+    if not resize_with_path:
+        image = tf.image.resize(image, size=image_shape, method='nearest') / 255
     
     num_objects = tf.shape(parsed_features['image/object/bbox/xmin'])[0]
     class_text = tf.sparse.to_dense(parsed_features['image/object/class/text'])
@@ -21,8 +47,10 @@ def _parse_features(example_proto, image_shape, features):
     xmax = tf.sparse.to_dense(parsed_features['image/object/bbox/xmax'])
     ymin = tf.sparse.to_dense(parsed_features['image/object/bbox/ymin'])
     ymax = tf.sparse.to_dense(parsed_features['image/object/bbox/ymax'])
-  
     bbox = tf.stack([ymin, xmin, ymax, xmax], axis=-1)
+
+    if resize_with_path:
+        image, bbox = if_resize_with_path(image, bbox, image_shape)
     return image, bbox
 
 
@@ -45,7 +73,7 @@ def load_voc_dataset(args, voc_path, augment_func = None, split = 'train'):
 
     dataset = tf.data.TFRecordDataset(voc_path)
     dataset = dataset.shuffle(args.buffer_size)
-    dataset = dataset.map(lambda x: _parse_features(x, args.img_size, useful_features), num_parallel_calls=AUTOTUNE)
+    dataset = dataset.map(lambda x: _parse_features(x, image_shape = args.img_size, features = useful_features), num_parallel_calls=AUTOTUNE)
     dataset = dataset.padded_batch(args.batch_size, drop_remainder=True)
     if (augment_func != None) and (split == 'train'):
         dataset = dataset.map(lambda x, y: augment_func((x, y)), num_parallel_calls=tf.data.AUTOTUNE) # x - image, y - (bbox, bbox, bbox, bbox)
@@ -75,7 +103,7 @@ def load_coco_dataset(args, coco_path, augment_func = None, split = 'train'):
 
     dataset = tf.data.TFRecordDataset(coco_path)
     dataset = dataset.shuffle(args.buffer_size)
-    dataset = dataset.map(lambda x: _parse_features(x, args.img_size, useful_features), num_parallel_calls=AUTOTUNE)
+    dataset = dataset.map(lambda x: _parse_features(x, image_shape = args.img_size, features = useful_features), num_parallel_calls=AUTOTUNE)
     dataset = dataset.padded_batch(args.batch_size, drop_remainder=True)
     if (augment_func != None) and (split == 'train'):
         dataset = dataset.map(lambda x, y: augment_func((x, y)), num_parallel_calls=tf.data.AUTOTUNE) # x - image, y - (bbox, bbox, bbox, bbox)
